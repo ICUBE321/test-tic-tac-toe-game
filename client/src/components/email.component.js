a5f75ac1a632bb7d73ccb4dac445e8deadbfbc23
ICUBE321 <47456033+ICUBE321@users.noreply.github.com>
2021-03-01 11:49:05 -0500

    fixed value error

--- client/src/components/email.component.js
+++ client/src/components/email.component.js
@@ -59,7 +59,7 @@ export default class Email extends Component {
         console.log('Message: ' + message);
         //console.log('Message from state: ' + this.state.message);
         //validate submit
-        if(this.state.username.trim().length < 1 || this.state.username === null || this.state.email == null || (Object.keys(value).length === 0 && this.state.email.constructor === Object)) {
+        if(this.state.username.trim().length < 1 || this.state.username === null || this.state.email == null || (Object.keys(this.state.email).length === 0 && this.state.email.constructor === Object)) {
             window.alert("Please fill in the required information to send!");
         } else {
             this.sendFeedback({user_name: this.state.username, user_email: this.state.email, message: message});
